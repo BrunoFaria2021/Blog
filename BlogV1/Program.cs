661f6afa87b54cc11f2a63da4a7d0255189f16b0
BrunoFaria2021 <bmc_ti@outlook.com>
2023-02-06 00:30:41 -0300

    Update BlogV2

--- BlogV1/Program.cs
+++ BlogV1/Program.cs
@@ -1,7 +1,30 @@
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder
     .Services
@@ -11,39 +34,24 @@ builder
         options.SuppressModelStateInvalidFilter = true;
     });
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>(); //Sempre ira criar uma nova instancia
+//builder.Services.AddScoped();Sempre ira dura por transação
+//builder.Services.AddSingleton();Singleton => 1 por App!
 
 // Adicione as seguintes linhas para adicionar o Swagger
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Blog API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Blog API", Version = "v2" });
 });
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Blog API V2");
 });
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-/*using Blog.Data;
-using Microsoft.AspNetCore.Mvc;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder
-    .Services
-    .AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-builder.Services.AddDbContext<BlogDataContext>();
-
-var app = builder.Build();
-app.MapControllers();
-
-app.Run();*/
